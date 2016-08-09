b19ee7243f3b2ef4db20f17cf38463a96c8c5057
longbai <slongbai@gmail.com>
2016-08-09 16:51:50 +0800

    fixed travis

--- install_cpputest.sh
+++ install_cpputest.sh
@@ -2,4 +2,4 @@
 
 wget https://github.com/cpputest/cpputest/releases/download/v3.8/cpputest-3.8.tar.gz -O /tmp/cpputest.tar.gz
 tar -xzvf /tmp/cpputest.tar.gz
-cd cpputest-3.8/scripts && sudo ./InstallScripts.sh
+cd cpputest-3.8/scripts && sh InstallScripts.sh
